7b06c8636d95a8d020bf56f8cef7f17bb1efc889
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-02-10 00:39:06 +0000

    This is the Main File

--- grubpacinstall.sh
+++ grubpacinstall.sh
@@ -4,14 +4,15 @@ echo "This Will Install GRUB and Make a Config File"
 echo "You have 10 Seconds to Exit if Needed (Ctrl+C)"
 sleep 10
 sudo su
+pacman -Syy
 pacman -Syu
-pacman -Syu grub ospober
-pacman -Syu grub
-pacman -Syu ospober
+pacman -S grub ospober
+pacman -S grub
+pacman -S ospober
 
-echo "What Drive and Partion?"
+echo "What Drive and Partion? You Have Twenty Seconds to Answer Before >
 read = part
-sleep 10
+sleep 20
 
 grub-install $part
 grub-mkconfig -o /boot/grub/grub.cfg
